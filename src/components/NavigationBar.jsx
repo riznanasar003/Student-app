6bb781797fa5015e7b8ec1ff6f9db411cb2e6c90
riznanasar003 <richurizna10@gmail.com>
2024-06-07 15:01:52 +0530

    changes succesfully

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavigationBar = () => {
   return (
     <div>
         <nav class="navbar navbar-expand-lg bg-warning">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">STUDENT APP</a>
+    <Link class="navbar-brand" to="#">STUDENT APP</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">ADD STUDENT</a>
+          <Link class="nav-link active" aria-current="page" to="/">ADD STUDENT</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/search">SEARCH</a>
+          <Link class="nav-link" to="/search">SEARCH</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/delete">REMOVE</a>
+          <Link class="nav-link" to="/delete">REMOVE</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/viewAll">VIEW ALL</a>
+          <Link class="nav-link" to="/viewAll">VIEW ALL</Link>
         </li>
       </ul>
     </div>
